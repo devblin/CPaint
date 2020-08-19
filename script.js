e68cc6bf8e000587b42ce1dc14074a4a14d74e24
Deepanshu Dhruw <deepanshu.dhruw@outlook.com>
2020-08-19 18:52:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,30 +27,44 @@ canvas.addEventListener("dblclick", function () {
     clear();
   }
 });
+var cX;
+var cY;
 canvas.addEventListener("mousedown", down);
-function down() {
+function down(e) {
   canvas.addEventListener("mousemove", move);
   canvas.addEventListener("mouseup", up);
+  cX = e.clientX - canvas.offsetLeft;
+  cY = e.clientY - canvas.offsetTop;
   function move(e) {
     pX = e.clientX - canvas.offsetLeft;
     pY = e.clientY - canvas.offsetTop;
     if (toolName == "pen") {
       draw(pX, pY, penColor, 2, 2);
-    } else {
-      draw(pX, pY, "white", 5, 5);
+    } else if (toolName == "eraser") {
+      erase(pX, pY);
     }
+    cX = e.clientX - canvas.offsetLeft;
+    cY = e.clientY - canvas.offsetTop;
   }
   function up() {
     canvas.removeEventListener("mousemove", move);
     canvas.removeEventListener("mouseup", up);
+    cX = undefined;
+    cY = undefined;
   }
 }
 function fill(color) {
   canvas.style.backgroundColor = color;
 }
-function draw(sX, sY, color, x, y) {
-  ctx.fillStyle = color;
-  ctx.fillRect(sX, sY, x, y);
+function draw(sX, sY, color) {
+  ctx.beginPath();
+  ctx.moveTo(cX, cY);
+  ctx.lineTo(sX, sY);
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+function erase(sX, sY) {
+  ctx.clearRect(sX, sY, 10, 10);
 }
 function clear() {
   ctx.clearRect(0, 0, 600, 400);
